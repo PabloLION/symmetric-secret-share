877d4066e0a0a427b3685979d8753c3275444e0f
Pablion <36828324+Pablion@users.noreply.github.com>
2022-02-03 16:19:29 +0100

    ref: codecs import, more test cases

--- sss_cli/encryption.py
+++ sss_cli/encryption.py
@@ -1,4 +1,4 @@
-import codecs
+from codecs import encode, decode
 import nacl.utils
 import nacl.secret
 import nacl.exceptions
@@ -10,12 +10,12 @@ def encrypt(message: str, key: str) -> str:
     Encrypt string with key
     """
     try:
-        box = nacl.secret.SecretBox(codecs.encode(key, "utf-8"))
+        box = nacl.secret.SecretBox(encode(key, "utf-8"))
     except nacl.exceptions.ValueError:
-        typer.secho("The key must be exactly 32 bytes long ", fg="red")
+        typer.secho("The key must be exactly 32-char long ", fg="red")
         raise typer.Exit(code=1)
-    encrypted = box.encrypt(codecs.encode(message, "utf-8"))
-    b64 = codecs.encode(encrypted, "base64").decode("utf-8")
+    encrypted = box.encrypt(encode(message, "utf-8"))
+    b64 = encode(encrypted, "base64").decode("utf-8")
     return b64
 
 
@@ -24,11 +24,11 @@ def decrypt(encrypted: str, key: str) -> str:
     Encrypt string with key
     """
     try:
-        box = nacl.secret.SecretBox(codecs.encode(key, "utf-8"))
+        box = nacl.secret.SecretBox(encode(key, "utf-8"))
     except nacl.exceptions.ValueError:
-        typer.secho("The key must be exactly 32 bytes long ", fg="red")
+        typer.secho("The key must be exactly 32-char long ", fg="red")
         raise typer.Exit(code=1)
-    byte_msg = codecs.decode(bytes(encrypted, "utf-8"), "base64")
+    byte_msg = decode(encode(encrypted, "utf-8"), "base64")
     try:
         decrypted = box.decrypt(byte_msg).decode("utf-8")
     except nacl.exceptions.CryptoError:
@@ -38,8 +38,11 @@ def decrypt(encrypted: str, key: str) -> str:
 
 
 def test():
-    fake_key = "I'm a sentence of 32 characters."
-    msg = "i'm a secret message, but not a sentence, very very very long lorem ipsum dolor, very very very long lorem ipsum dolor, very very very long lorem ipsum dolor, very very very long lorem ipsum dolor, very very very long lorem ipsum dolor, very very very long lorem ipsum dolor, very very very long lorem ipsum dolor, very very very long lorem ipsum dolor"
+    fake_key = "I'm a sentence of 32 characters."  # 32 one-byte characters
+    fake_key = "4}M?XW]:`TqSP9/m2RW]l-Tatx0bFVa,"  # 32 one-byte characters
+    fake_key = "ǕèǶ¿§ܚøө½ߊƎǏƵǪИ"  # 32 two-byte characters
+    fake_key = "ݒȄԸưŘ͎ɡҜȐѽژ̎ʶܐڝؔ"
+    msg = "I'm a secret message."
 
     encrypted = encrypt(msg, fake_key)
     decrypted = decrypt(encrypted, fake_key)
